275c0c6478b7af28897758bc60a47e7600599092
Spencer Harris <harris.spencer63@gmail.com>
2024-03-29 16:53:21 -0400

    Fixed DB_URI issue hopefully

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('sqlite:///posts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', 'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
